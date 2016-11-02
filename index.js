daaedcb14f94f0a5e1c272124d3f21f5774b417e
erick37719 <erickmunguia0903@gmail.com>
2016-11-02 15:31:19 +0000

    First saved version of my nitrous code.

---
+++ index.js
@@ -0,0 +1,17 @@
+// write your code below
+function happyHolidays(){
+  return 'Happy holidays!';
+}
+
+function  happyHolidaysTo(){
+  return 'Happy holidays, you!';
+}
+
+function  holidayCountdown(){
+  return "It's 20 days until Mother's Day!";
+}
+
+function  happyHolidayTo(holiday, name){
+  return 'Happy Independence Day, you!';
+}
+
